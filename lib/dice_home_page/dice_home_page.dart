6f227f6f1c90f5fb4b7dd1e5d6168754adfe2ed1
AstroMindinfotech <100206022+AstroMindinfotech@users.noreply.github.com>
2023-05-27 18:29:01 +0400

    unwanted comment removed

--- lib/dice_home_page/dice_home_page.dart
+++ lib/dice_home_page/dice_home_page.dart
@@ -36,18 +36,6 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // child: Column(
-        //   mainAxisAlignment: MainAxisAlignment.center,
-        //   children: <Widget>[
-        //     const Text(
-        //       'You have pushed the button this many times:',
-        //     ),
-        //     Text(
-        //       '$_counter',
-        //       style: Theme.of(context).textTheme.headlineMedium,
-        //     ),
-        //   ],
-        // ),
         child: OutlinedButton(
           onPressed: () {
             _incrementCounter();
@@ -55,11 +43,6 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Image.asset('images/dice$_counter.png'),
         ),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.gamepad),
-      // ),
     );
   }
 }
